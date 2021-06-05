37694288bd156e8996c733b8921bb771cac46373
huy <huy@gmail.com>
2021-06-05 22:15:28 +0700

    Upload assigment duongpho daoquanghuy1 Th2009022 T2009M1

---
+++ StreetCRUD/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using StreetCRUD.controller;
+using StreetCRUD.entity;
+using StreetCRUD.model;
+using StreetCRUD.view;
+
+namespace StreetCRUD
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DuongPho duongPho = new DuongPho();
+            duongPho.Ma = "A001";
+            duongPho.Ten = "Nguyễn Văn Trác";
+            duongPho.MoTa = "Dương Nội";
+            duongPho.NgaySuDung = DateTime.Now ;
+            duongPho.LichSu = "Yên Nghĩa";
+            duongPho.TenQuan = "Hà Đông";
+            duongPho.TrangThai = 1;
+            DuongPhoDbModel duongPhoDbModel = new DuongPhoDbModel();
+            duongPhoDbModel.Save(duongPho);
+            //để sau đi up git nhé
+            //nó bị lỗi phông suwa đc k nhỉ lỗi đâu ?
+            // chỉnh đc nhưng để lần sau lúc tạo mới database thì để nó về utf8bm4_unicode_ci là đc uk
+            // up git nhé ôk
+        }
+    }
+}
