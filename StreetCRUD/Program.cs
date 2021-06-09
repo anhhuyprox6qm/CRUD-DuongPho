8b0999292289e3edaed26c07e88e2023d5689e48
anhhuyprox6qm <73454962+anhhuyprox6qm@users.noreply.github.com>
2021-06-09 08:40:33 +0700

    Update Program.cs

--- StreetCRUD/Program.cs
+++ StreetCRUD/Program.cs
@@ -21,10 +21,6 @@ namespace StreetCRUD
             duongPho.TrangThai = 1;
             DuongPhoDbModel duongPhoDbModel = new DuongPhoDbModel();
             duongPhoDbModel.Save(duongPho);
-            //để sau đi up git nhé
-            //nó bị lỗi phông suwa đc k nhỉ lỗi đâu ?
-            // chỉnh đc nhưng để lần sau lúc tạo mới database thì để nó về utf8bm4_unicode_ci là đc uk
-            // up git nhé ôk
         }
     }
 }
